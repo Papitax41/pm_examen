6cb98c5ec70e1458377c9bd69912095e375ecb98
Papita <202000672k@utea.edu.pe>
2023-09-22 07:01:27 -0500

    first commit

---
+++ app_state/lib/test2_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Test2Page extends StatefulWidget {
+  const Test2Page({super.key});
+
+  @override
+  State<Test2Page> createState() => _Test2PageState();
+}
+
+class _Test2PageState extends State<Test2Page> {
+  bool isDescriptionJustify = false;
+  double valueRedSlider = 0;
+  double valueGreenSlider = 0;
+  double valueBlueSlider = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("IMC App"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text(
+              "Bienvenido, selecciona tu peso y altura",
+              style: TextStyle(
+                color: Color.fromRGBO(valueGreenSlider.toInt(), valueBlueSlider.toInt(),120,1),
+              ),
+            ),
+
+            Slider(
+              min: 0,
+              max: 255,
+              value: valueGreenSlider,
+              onChanged: (value) {
+                print(value);
+                valueGreenSlider = value;
+                setState(() {});
+              },
+            ),
+            Slider(
+              min: 0,
+              max: 255,
+              value: valueBlueSlider,
+              label: valueBlueSlider.round().toString(),
+              onChanged: (value) {
+                print(value);
+                valueBlueSlider = value;
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
