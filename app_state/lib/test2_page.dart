af457475d538fccb34f3b99f194afc09fa8da5cf
Papita <202000672k@utea.edu.pe>
2023-09-22 07:52:53 -0500

    3er commit

--- app_state/lib/test2_page.dart
+++ app_state/lib/test2_page.dart
@@ -8,8 +8,9 @@ class Test2Page extends StatefulWidget {
 }
 
 class _Test2PageState extends State<Test2Page> {
-  double valueGreenSlider = 0;
-  double valueBlueSlider = 0;
+  double peso = 0;
+  double altura = 0;
+  double imc = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -27,32 +28,64 @@ class _Test2PageState extends State<Test2Page> {
               "Bienvenido, selecciona tu peso y altura",
             ),
             SizedBox(height: 20),
-            Text(
-              "${valueGreenSlider.toStringAsFixed(0)} Kg",
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "${peso.toStringAsFixed(0)}",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  "kg",
+                ),
+              ],
             ),
             Slider(
               min: 0,
               max: 255,
-              value: valueGreenSlider,
+              value: peso,
               onChanged: (value) {
                 setState(() {
-                  valueGreenSlider = value;
+                  peso = value;
                 });
               },
             ),
             SizedBox(height: 20),
-            Text(
-              "${valueBlueSlider.toStringAsFixed(0)} Cm",
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "${altura.toStringAsFixed(0)}",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  "cm",
+                ),
+              ],
+            ),
             Slider(
               min: 0,
               max: 255,
-              value: valueBlueSlider,
+              value: altura,
               onChanged: (value) {
                 setState(() {
-                  valueBlueSlider = value;
+                  altura = value;
                 });
               },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                imc = peso / (altura * altura); // Realiza el cálculo del IMC al presionar el botón
+                });
+              },
+              child: Text("Calcular IMC"),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "IMC: ${imc.toStringAsFixed(2)}", // Muestra el resultado del IMC
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
